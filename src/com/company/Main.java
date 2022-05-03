82832e3cc207af8e79f55ac2df48247336c703a5
olbe0615 <oleg.berest@NetCracker.com>
2022-05-03 09:17:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    //psvm
+    public static void main(String[] args) {
+
+        //sout
+        System.out.println("Hello world!");
+
+    }
+
+
+}
